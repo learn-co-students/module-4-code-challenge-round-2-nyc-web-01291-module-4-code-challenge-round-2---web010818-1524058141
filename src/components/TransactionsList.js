ba0a574ea1c3f733a55c105466a721d4a56496d4
Zahran Kabir <zahran@Zahrans-MacBook-Pro.local>
2018-04-18 10:13:41 -0400

    done

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,38 +1,55 @@
 import React from 'react'
+import Transaction from './Transaction'
 
-const TransactionsList = () => {
+class TransactionsList extends React.Component {
 
-  return (
-    <table className="ui celled striped padded table">
-      <tbody>
-        <tr>
-          <th>
-            <h3 className="ui center aligned header">
-              Posted At
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Description
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Category
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Amount
-            </h3>
-          </th>
-        </tr>
+  constructor(props){
+    super(props)
+    console.log(props)
+  }
 
-        {"... your code here..."}
+  renderTransaction = () => {
+    return this.props.transactions.map(transaction => <Transaction key={transaction.id} transaction={transaction} />)
+  }
 
-      </tbody>
-    </table>
-  )
+  render(){
+    return (
+      <div>
+      Search: <input onChange={this.props.handleChange} type="text"/>
+      <table className="ui celled striped padded table">
+        <tbody>
+          <tr>
+            <th>
+              <h3 className="ui center aligned header">
+                Posted At
+              </h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">
+                Description
+              </h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">
+                Category
+              </h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">
+                Amount
+              </h3>
+            </th>
+          </tr>
+
+          {this.renderTransaction()}
+
+
+        </tbody>
+      </table>
+      </div>
+
+    )
+  }
 }
 
 export default TransactionsList
